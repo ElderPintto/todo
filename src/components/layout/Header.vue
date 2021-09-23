224eebde060d9e31b4dbe188a2e90979d81ca297
ElderPintto <eldersilva@gmail.com>
2021-09-23 21:48:56 +0100

    seprando compomentes

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <Navigation :drawer="drawer" :items="items" />
+
+    <v-app-bar
+        app
+        color="primary"
+        dark
+        shrink-on-scroll
+        src="https://picsum.photos/1920/1080?random"
+        prominent
+    >
+        <template v-slot:img="{ props }">
+        <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+        </template>
+
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-app-bar-title>Todo list</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+    </v-app-bar>
+
+  </header>
+</template>
+
+<script>
+import Navigation from './Navigation.vue'
+export default {
+  name: 'Header',
+  components: {
+    Navigation
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+    right: null,
+  }),
+};
+</script>
+
+<style></style>
---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6">
+          Todo List
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          Best Todo App
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {
+    drawer: {
+      type: Boolean,
+      default: () => null,
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data: () => ({}),
+};
+</script>
+
+<style></style>
